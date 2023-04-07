eb37d94db35c384afa4453d430566fcd35781f46
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2023-04-07 04:53:39 +0200

    feat: add mobile menu

--- shelter/src/js/modules/Menu.js
+++ shelter/src/js/modules/Menu.js
@@ -1,6 +1,9 @@
 class Menu {
   constructor() {
     this.button = document.getElementById(`menu-button`)
+    this.nav = document.getElementById(`header-nav`)
+    this.menu = document.getElementById(`main-menu`)
+    this.isOpen = false
 
     this._initListeners()
   }
@@ -9,10 +12,34 @@ class Menu {
     this.button && this.button.addEventListener(`click`, () => {
       this._toggleNav()
     })
+
+    this.nav && (this.overlay = document.querySelector(`.header-nav__overlay`))
+    this.overlay && this.overlay.addEventListener(`click`, () => {
+      this._toggleNav()
+    })
+
+    this.menu && this.menu.addEventListener(`click`, (e) => {
+      console.log(e.target.classList)
+      if (e.target.classList.contains(`main-menu__link`)) {
+        this._toggleNav()
+      }
+    })
+  }
+
+  _disableScroll() {
+    document.body.style.overflow = `hidden`
+  }
+
+  _enableScroll() {
+    document.body.style.overflow = `visible`
   }
 
   _toggleNav() {
+    this.isOpen = !this.isOpen
+
+    this.isOpen ? this._disableScroll() : this._enableScroll()
     this.button.classList.toggle(`menu-button--open`)
+    this.nav.classList.toggle(`header-nav--open`)
   }
 }
 
